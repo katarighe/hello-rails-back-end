dfa29d497a509d4e23077edf6b43faeac0cbb518
katarighe <m.ighe@hotmail.com>
2023-11-29 05:12:03 +0300

    Update the message controller test

--- test/controllers/api/v1/messages_controller_test.rb
+++ test/controllers/api/v1/messages_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class Api::V1::MessagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should get index' do
+    get messages_index_url
+    assert_response :success
+  end
 end
